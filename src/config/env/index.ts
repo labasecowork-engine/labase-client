41802c399ba1df090c6fbc69a2e66abac574d21f
YerikAH <yerik05vh@gmail.com>
2026-02-20 14:04:44 -0500

    chore: :recycle: Remove unused NIUBIZ configuration variables
    - Deleted NIUBIZ_MERCHANT_ID and NIUBIZ_URL_JS from the environment configuration as they are no longer needed.

--- src/config/env/index.ts
+++ src/config/env/index.ts
@@ -3,5 +3,3 @@ export const SOCKET_URL =
   import.meta.env.VITE_SOCKET_URL || "ws://localhost:3000";
 export const API_URL =
   import.meta.env.VITE_API_URL || "http://localhost:3000/api/v1";
-export const NIUBIZ_MERCHANT_ID = import.meta.env.VITE_NIUBIZ_MERCHANT_ID;
-export const NIUBIZ_URL_JS = import.meta.env.VITE_NIUBIZ_URL_JS;
